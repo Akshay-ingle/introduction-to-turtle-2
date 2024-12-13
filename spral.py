52656604137f004be2db1740ee185a2c93ac5075
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2024-12-13 19:42:05 +0530

    spiral2

--- spral.py
+++ spral.py
@@ -1,11 +1,13 @@
-import turtle    
+import turtle  
+
 mn=turtle.Screen()
 mn.bgcolor("light blue") 
 mn.title("Turtle")
 size = 0
+mg=turtle.Turtle()
 while True: 
     for i in range(4): 
-        mn.fd(size + 1)
-        mn.left(90)
+        mg.fd(size + 1)
+        mg.left(90)
         size = size - 5
     size = size + 1    
